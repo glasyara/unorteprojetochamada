17475e50fa32c4052ca587db9c1e934bbfb17877
glasyara <139944286+glasyara@users.noreply.github.com>
2023-11-07 23:35:20 +0000

    Update students.json and controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -1,11 +1,14 @@
 import * as fs from "fs";
 
-const filePath = "/data/students.json";
+const filePath = "./data/students.json";
 
 const data = "Hello World!!";
 
 function helloWorld() {
-  fs.writeFileSync(filePath, data, "utf8");
+  const data = fs.readFileSync(filePath, "utf8");
+  return data;
+
+  //fs.writeFileSync(filePath, data, "utf8");
 }
 
 export { helloWorld };
